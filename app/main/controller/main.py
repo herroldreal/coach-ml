45051bc3c3ee5467ad1c7f1ff51ba77a5831190c
Herrold Real <herrold.real@go1.com>
2023-02-07 11:20:26 -0600

    fix(git):
    :hammer: remove file from .gitignore

--- app/main/controller/main.py
+++ app/main/controller/main.py
@@ -1,4 +1,4 @@
-from model import verify
+from service.model import verify
 
 try:
     image_path1 = 'picture/5.jpg'
--- app/main/service/model.py
+++ app/main/service/model.py
@@ -8,17 +8,27 @@ def verify(image_path1, image_path2):
         enforce_detection=False,
         detector_backend='mtcnn'
     )
-    #  df = DeepFace.find(image_path1, './', enforce_detection=False)
-    # analyze = DeepFace.analyze(
-    #     image_path1,
-    #     enforce_detection=True,
-    #     detector_backend='mtcnn',
-    #     actions=('age', 'gender', 'race', 'emotion')
-    # )
 
-    #  print('Face Recognition result -> {}'.format(df))
-    #  print('Face result -> {}'.format(result))
-    analyze = result
-    print(analyze)
-    # print('Face emotion result -> Gender: {} - Age: {} - Race: {} - Emotion: {}'.format(analyze["dominant_gender"],
-    # analyze["age"], analyze["dominant_race"], analyze["dominant_emotion"]))
+    print('Face emotion result -> Gender: {} - Age: {} - Race: {} - Emotion: {}'.format(
+        result[0]["dominant_gender"],
+        result[0]["age"],
+        result[0]["dominant_race"],
+        result[0]["dominant_emotion"]
+    ))
+
+
+def find(image_path):
+    df = DeepFace.find(image_path, './', enforce_detection=False)
+    print('Face Recognition result -> {}'.format(df))
+
+
+def analyze(image_path):
+    analyze = DeepFace.analyze(
+         image_path,
+         enforce_detection=True,
+         detector_backend='mtcnn',
+         actions=('age', 'gender', 'race', 'emotion')
+    )
+
+    print('Face result -> {}'.format(analyze))
+
